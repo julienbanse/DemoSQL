7f2015c939cbe216e7e57d6ff3789178345d1629
Julien Banse <julien.banse@gmail.com>
2017-11-08 23:18:49 +0100

    create view model to interact with news list item in db

---
+++ app/src/main/java/sample/jbanse/demosql/ui/MainViewModel.kt
@@ -0,0 +1,76 @@
+package sample.jbanse.demosql.ui
+
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.ViewModel
+import android.util.Log
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import sample.jbanse.demosql.data.controller.Repository
+import sample.jbanse.demosql.data.controller.model.NewsListItem
+import java.util.Date
+import javax.inject.Inject
+
+/**
+ * Created by julien on 08/11/2017.
+ */
+class MainViewModel
+@Inject
+constructor(private val repository: Repository) : ViewModel() {
+
+    private val TAG = "MainViewModel"
+
+    var sortByDate = true
+        set(value) {
+            if (field != value) {
+                field = value
+                loadData()
+            }
+        }
+
+    private val disposables = CompositeDisposable()
+
+    private val newsList = MutableLiveData<List<NewsListItem>>()
+
+    init {
+        loadData()
+    }
+
+    private fun loadData() {
+        if (sortByDate) loadNewsOrderByDate() else loadNewsOrderByPosition()
+    }
+
+    fun addItem(count: Int) {
+        disposables.add(
+                repository.insertNews("title $count", Date(), count)
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe({ Log.i(TAG, "item added") },
+                                { Log.e(TAG, "item add error", it) })
+        )
+    }
+
+    fun newsListLiveData(): LiveData<List<NewsListItem>> = newsList
+
+    private fun loadNewsOrderByDate() {
+        disposables.clear()
+        disposables.add(repository.selectNewsOrderByDate()
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({ newsList.postValue(it) },
+                        { Log.d(TAG, "news oder by date", it) })
+        )
+    }
+
+    private fun loadNewsOrderByPosition() {
+        disposables.clear()
+        disposables.add(repository.selectNewsOrderByPosition()
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({ newsList.postValue(it) },
+                        { Log.d(TAG, "news oder by date", it) }))
+    }
+
+    override fun onCleared() {
+        Log.d(TAG, "onCleared")
+        super.onCleared()
+        disposables.clear()
+    }
+}
