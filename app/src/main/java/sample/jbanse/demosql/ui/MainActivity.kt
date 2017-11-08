9b2890c86e6200a3d64d0552751a95fd919c6272
Julien Banse <julien.banse@gmail.com>
2017-11-08 23:22:58 +0100

    use view model to get news from db

--- app/src/main/java/sample/jbanse/demosql/ui/MainActivity.kt
+++ app/src/main/java/sample/jbanse/demosql/ui/MainActivity.kt
@@ -1,43 +1,35 @@
 package sample.jbanse.demosql.ui
 
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProvider
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v7.widget.RecyclerView
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.Toast
 import dagger.android.support.DaggerAppCompatActivity
-import io.reactivex.Observer
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.disposables.Disposable
 import sample.jbanse.demosql.R
-import sample.jbanse.demosql.data.controller.Repository
 import sample.jbanse.demosql.data.controller.model.NewsListItem
-import java.util.Date
 import javax.inject.Inject
 
 
-class MainActivity : DaggerAppCompatActivity(), Observer<List<NewsListItem>> {
+class MainActivity : DaggerAppCompatActivity() {
 
     private val SAVE_STATE_SORT_BY_DATE = "IS_SORT_BY_DATE"
-
-    private var sortByDate = true
-
-    private val compositeDisposable = CompositeDisposable()
-
-    private var currentDisposable: Disposable? = null
-
-    @Inject lateinit var repository: Repository
 
     private val recycler: RecyclerView by lazy { findViewById<RecyclerView>(R.id.mainRv) }
 
     private val adapter: NewsListAdapter by lazy { NewsListAdapter(this) }
 
+    @Inject lateinit var viewModelFactory: ViewModelProvider.Factory
+
+    private lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         recycler.adapter = adapter
+        viewModel = ViewModelProviders.of(this, viewModelFactory).get(MainViewModel::class.java)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -49,11 +41,11 @@ class MainActivity : DaggerAppCompatActivity(), Observer<List<NewsListItem>> {
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
         return when (item?.itemId) {
             R.id.addItem -> {
-                addItem()
+                viewModel.addItem(adapter.itemCount)
                 true
             }
             R.id.modifySort -> {
-                sortByDate = !sortByDate
+                viewModel.sortByDate = !viewModel.sortByDate
                 observeList()
                 true
             }
@@ -63,12 +55,12 @@ class MainActivity : DaggerAppCompatActivity(), Observer<List<NewsListItem>> {
 
     override fun onSaveInstanceState(outState: Bundle?) {
         super.onSaveInstanceState(outState)
-        outState?.putBoolean(SAVE_STATE_SORT_BY_DATE, sortByDate)
+        outState?.putBoolean(SAVE_STATE_SORT_BY_DATE, viewModel.sortByDate)
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
         super.onRestoreInstanceState(savedInstanceState)
-        sortByDate = savedInstanceState?.getBoolean(SAVE_STATE_SORT_BY_DATE, true) ?: true
+        viewModel.sortByDate = savedInstanceState?.getBoolean(SAVE_STATE_SORT_BY_DATE, true) ?: true
     }
 
     override fun onStart() {
@@ -76,49 +68,8 @@ class MainActivity : DaggerAppCompatActivity(), Observer<List<NewsListItem>> {
         observeList()
     }
 
-    override fun onStop() {
-        super.onStop()
-        compositeDisposable.clear()
-    }
-
     private fun observeList() {
-        //remove previous disposable to listen only the current request.
-        currentDisposable?.let { compositeDisposable.delete(it) }
-        if (sortByDate) {
-            repository.selectNewsOrderByDate()
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe(this)
-        } else {
-            repository.selectNewsOrderByPosition()
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe(this)
-        }
-    }
-
-    private fun addItem() {
-        compositeDisposable.add(
-                repository.insertNews("title ${adapter.itemCount}", Date(), adapter.itemCount)
-                        .observeOn(AndroidSchedulers.mainThread())
-                        .subscribe({ Log.i("MAIN", "item added") },
-                                { Log.e("MAIN", "item add error", it) })
-        )
-    }
-
-    override fun onNext(t: List<NewsListItem>) {
-        adapter.updateData(t)
-    }
-
-    override fun onComplete() {
-
-    }
-
-    override fun onSubscribe(d: Disposable) {
-        compositeDisposable.add(d)
-        currentDisposable = d
-    }
-
-    override fun onError(e: Throwable) {
-        Log.e("MAIN", "observeList", e)
-        Toast.makeText(this, "Error: " + e.message, Toast.LENGTH_LONG).show()
+        viewModel.newsListLiveData().observe(this,
+                Observer<List<NewsListItem>> { t -> adapter.updateData(t ?: emptyList()) })
     }
 }
--- app/src/main/java/sample/jbanse/demosql/ui/MainBuilder.kt
+++ app/src/main/java/sample/jbanse/demosql/ui/MainBuilder.kt
@@ -1,7 +1,11 @@
 package sample.jbanse.demosql.ui
 
+import android.arch.lifecycle.ViewModel
+import dagger.Binds
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
+import dagger.multibindings.IntoMap
+import sample.jbanse.demosql.inject.ViewModelKey
 
 /**
  * Created by julien on 17/09/2017.
@@ -11,4 +15,11 @@ internal abstract class MainBuilder {
 
     @ContributesAndroidInjector
     abstract fun bindActivity(): MainActivity
+
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(MainViewModel::class)
+    abstract fun bindMainActivityViewModel(viewModel: MainViewModel): ViewModel
+
 }
